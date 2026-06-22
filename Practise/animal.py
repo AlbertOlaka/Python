a474d1be932d517294a032c5c4597e45f1cf5d81
AlbertOlaka <olaka.aj@gmail.com>
2026-06-22 17:53:23 +0300

    Started chapter 4, working with list

---
+++ Practise/animal.py
@@ -0,0 +1,6 @@
+favourite_pet = ['dog', 'cat', 'parrot']
+for pet in favourite_pet:
+    print(pet)
+    print(f"A {pet.title()}, would make a great pet")
+
+print("Any of these animals would make great pets.")
---
+++ Practise/pizza.py
@@ -0,0 +1,6 @@
+favourite_pizaa = ['chicken tikka', 'peri peri chicken', 'bbq beef']
+for pizza in favourite_pizaa:
+    print(pizza)
+    print(f"I love the {pizza.title()} pizza")
+
+print("I really like having pizza, especially on the days when their are offers on Tuesday when it but one get one free. I really love pizza.")
