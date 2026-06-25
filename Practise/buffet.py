d8550b71bf29c92f8023a59b4dfca706b4257c98
AlbertOlaka <olaka.aj@gmail.com>
2026-06-25 16:18:48 +0300

    Finished Chapter 4

---
+++ Practise/buffet.py
@@ -0,0 +1,8 @@
+buffets = ('chicken curry', 'beef curry', 'pilau', 'biriyani', 'chips')
+for buffet in buffets:
+    print(buffet)
+
+buffets = ('chicken curry', 'chapati', 'pilau', 'biriyani', 'ugali')
+print("\nBelow is our new menu:")
+for buffet in buffets:
+    print(buffet)
