91524c8f4f750e7293115e16088b2f09a20279e5
AlbertOlaka <olaka.aj@gmail.com>
2026-06-29 11:22:12 +0300

    Done with if simple if statements, if-else, if-elif-else

---
+++ If Statements/voting.py
@@ -0,0 +1,14 @@
+# Simple if statements.
+age = 18
+if age >= 18:
+    print("Your are old enough to vote.")
+    print("Have you registered to vote yet?")
+
+# If-else statement.
+age = 17
+if age >= 18:
+    print("Your are old enough to vote.")
+    print("Have you registered to vote yet?")
+else:
+    print("Sorry, you are too young to vote.")
+    print("Please register to vote as sson as you turn 18!")
