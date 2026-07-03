1d1b2e90256aae831f2008110c2039ee506daecd
AlbertOlaka <olaka.aj@gmail.com>
2026-07-03 23:37:17 +0300

    Start on the exercise on page 201

--- Dictionaries/favorite_languages.py
+++ Dictionaries/favorite_languages.py
@@ -5,5 +5,51 @@ favorite_languages = {
     'phil' : 'python',
 }
 
-language = favorite_languages['sarah'].title()
-print(f"Sarah's favorite language is {language}")
+for name, language in favorite_languages.items():
+    print(f"{name.title()}'s favorite language is {language.title()}")
+
+# Using the .keys() method.
+for name in favorite_languages.keys():
+    print(name.title())
+
+# Printing a personalized message to specific friends.
+favorite_languages = {
+    'jen' : 'python',
+    'sarah' : 'c',
+    'edward' : 'rust',
+    'phil' : 'python',
+}
+
+friends = ['phil', 'sarah']
+for name in favorite_languages.keys():
+    print(f"Hi {name.title()}.")
+    if name in friends:
+        language = favorite_languages[name].title()
+        print(f"\t{name.title()}, I see you love {language}!")
+
+# Finding out if a particular person was polled
+if 'erin' not in favorite_languages.keys():
+    print("Erin, please take our poll!")
+
+# Using the sorted() function.
+favorite_languages = {
+    'jen' : 'python',
+    'sarah' : 'c',
+    'edward' : 'rust',
+    'phil' : 'python',
+}
+
+for name in sorted(favorite_languages.keys()):
+    print(f"\n{name.title()}, thank you for taking the poll.")
+
+# Using the values() method.
+favorite_languages = {
+    'jen' : 'python',
+    'sarah' : 'c',
+    'edward' : 'rust',
+    'phil' : 'python',
+}
+
+print("The following languages have been mentioned:")
+for language in set(favorite_languages.values()):
+    print(language.title())
