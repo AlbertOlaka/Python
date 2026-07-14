3e00575c85d320dc8d275bd3d2aa0405e46864f7
AlbertOlaka <olaka.aj@gmail.com>
2026-07-14 13:53:54 +0300

    Started with classes

---
+++ Classes/dog.py
@@ -0,0 +1,12 @@
+class Dog:
+    """A simple attempt to model a dog."""
+    def __init__(self, name, age):
+        """Initialize name & age attributes"""
+        self.name = name
+        self.age = age
+    def sit(self):
+        """Simulate a dog sitting in response to a command"""
+        print(f"{self.name} is now sitting")
+    def roll_over(self):
+        """Simulating a dog rolling over in response to a command"""
+        print(f"{self.name} rolled over!")
