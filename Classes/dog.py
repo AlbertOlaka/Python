bf9373efab4f63df000cc271f8211e6e86ea3d5e
AlbertOlaka <olaka.aj@gmail.com>
2026-07-16 22:48:59 +0300

    Started making instances of classes

--- Classes/dog.py
+++ Classes/dog.py
@@ -10,3 +10,8 @@ class Dog:
     def roll_over(self):
         """Simulating a dog rolling over in response to a command"""
         print(f"{self.name} rolled over!")
+
+# Making an Instances of a class
+my_dog = Dog('Willie', 6)
+print(f"My dog's name is {my_dog.name}.")
+print(f"My dog is {my_dog.age} years old.")
