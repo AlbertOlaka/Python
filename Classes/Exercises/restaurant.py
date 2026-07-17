bf46fbcd4d1405cc00576534b72d6cca6b675073
AlbertOlaka <olaka.aj@gmail.com>
2026-07-18 01:33:15 +0300

    Starting: Inheritance

--- Classes/Exercises/restaurant.py
+++ Classes/Exercises/restaurant.py
@@ -2,21 +2,41 @@ class Restaurant:
     def __init__(self, restaurant_name, cuisine_type):
         self.restaurant_name = restaurant_name
         self.cuisine_type = cuisine_type
+        self.served = 0
 
     def describe_restaurant(self):
         print(f"The name of my restaurant is {self.restaurant_name}.")
         print(f"It serves {self.cuisine_type} food.")
+
     def open_restaurant(self):
         print(f"{self.restaurant_name} is now open till midnight.")
+
+    def number_served(self):
+        """Print a statement of the number of people served in this restaurant"""
+        print(f"This restaurant has served {self.served} number of people.")
+
+    def set_number_served(self, people):
+        """Set the number of people served a the given value"""
+        self.served = people
+
+    def increment_number_served(self, day):
+        """Add the given amount to the number fo people served."""
+        self.served += day
 
 restaurant = Restaurant('Le Chateau', 'French cuisine')
 restaurant.describe_restaurant()
 restaurant.open_restaurant()
 
-italian_restaurant = Restaurant('La Dolce Vita', 'Italian cuisine')
-italian_restaurant.describe_restaurant()
-italian_restaurant.open_restaurant()
+restaurant.set_number_served(250)
+restaurant.number_served()
 
-spanish_restaurant = Restaurant('Boca Chica', 'Spanish cuisine')
-spanish_restaurant.describe_restaurant()
-spanish_restaurant.open_restaurant()
+restaurant.increment_number_served(100)
+restaurant.number_served()
+
+# italian_restaurant = Restaurant('La Dolce Vita', 'Italian cuisine')
+# italian_restaurant.describe_restaurant()
+# italian_restaurant.open_restaurant()
+
+# spanish_restaurant = Restaurant('Boca Chica', 'Spanish cuisine')
+# spanish_restaurant.describe_restaurant()
+# spanish_restaurant.open_restaurant()
--- Classes/Exercises/users.py
+++ Classes/Exercises/users.py
@@ -4,25 +4,52 @@ class User:
         self.last_name = last_name
         self.email = email
         self.phone_number = phone_number
+        self.login_attempts = 0
+
     def describe_user(self):
+        """Describing the user."""
         print(f"The users name is {self.first_name} {self.last_name}.")
         print(f"The users email is {self.email}")
         print(f"The users phone number is {self.phone_number}")
+
     def greet_user(self):
+        """greeting the user!"""
         print(f"Hello {self.first_name} {self.last_name}!")
+
+    def read_login(self):
+        """Print a statement showing the users login attempts."""
+        print(f"{self.first_name} {self.last_name} has attempted to login {self.login_attempts}.")
+
+    def increment_login_attempts(self):
+        self.login_attempts += 1
+
+    def reset_login_attempts(self):
+        """Reseting login attempts to zero."""
+        self.login_attempts = 0
 
 user = User('Willie', 'John', 'wjohn@gmail.com', 123456)
 user.describe_user()
 user.greet_user()
 
-user_1 = User('Lucy', 'Jow', 'ljow@gmail.com', 987655)
-user_1.describe_user()
-user_1.greet_user()
+user.increment_login_attempts()
+user.increment_login_attempts()
+user.increment_login_attempts()
+user.read_login()
 
-user_2 = User('Jane', 'Joe', 'jj@gmail.com', 876554)
-user_2.describe_user()
-user_2.greet_user()
+user.reset_login_attempts()
+user.read_login()
 
-user_3 = User('John', 'Doe', 'jdoe@gmail.com', 456781)
-user_3.describe_user()
-user_3.greet_user()
+user.read_login()
+
+
+# user_1 = User('Lucy', 'Jow', 'ljow@gmail.com', 987655)
+# user_1.describe_user()
+# user_1.greet_user()
+
+# user_2 = User('Jane', 'Joe', 'jj@gmail.com', 876554)
+# user_2.describe_user()
+# user_2.greet_user()
+
+# user_3 = User('John', 'Doe', 'jdoe@gmail.com', 456781)
+# user_3.describe_user()
+# user_3.greet_user()
