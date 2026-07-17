bc7385122cf7bcc1dc82709aa04d35f87b1ebaba
AlbertOlaka <olaka.aj@gmail.com>
2026-07-17 11:17:12 +0300

    Start on the exercise

--- Classes/dog.py
+++ Classes/dog.py
@@ -13,5 +13,12 @@ class Dog:
 
 # Making an Instances of a class
 my_dog = Dog('Willie', 6)
+your_dog = Dog('Lucy', 3)
+
 print(f"My dog's name is {my_dog.name}.")
-print(f"My dog is {my_dog.age} years old.")
+print(f"My dpg is {my_dog.age} years old.")
+my_dog.sit()
+
+print(f"My dog's name is {your_dog.name}.")
+print(f"My dpg is {your_dog.age} years old.")
+your_dog.sit()
