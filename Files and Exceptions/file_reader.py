61c3c3c5e0d863ecade4d1f9ae25add81765ca36
AlbertOlaka <olaka.aj@gmail.com>
2026-07-22 23:29:29 +0300

    Start: Writing to a File

--- Files and Exceptions/file_reader.py
+++ Files and Exceptions/file_reader.py
@@ -2,4 +2,20 @@ from pathlib import Path
 
 path = Path('pi_digits.txt')
 contents = path.read_text()
-print(contents)
+
+for line in contents.splitlines():
+    print(line)
+
+# Working with a File's Contents
+# from pathlib import Path
+
+# path = Path('pi_digits.txt')
+# contents = path.read_text()
+
+# lines = contents.splitlines()
+# pi_strings = ''
+# for line in lines:
+#     pi_strings += line.lstrip()
+
+# print(pi_strings)
+# print(len(pi_strings))
