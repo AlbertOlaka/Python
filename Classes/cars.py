077162ddf9b05b3f39602e45a74ea233df604f67
AlbertOlaka <olaka.aj@gmail.com>
2026-07-22 18:36:56 +0300

    Done with Classes

--- Classes/cars.py
+++ Classes/cars.py
@@ -62,12 +62,12 @@ class ElectricCar(Car):
         self.battery = Battery()
         
 
-my_leaf = ElectricCar('nissan', 'leaf', 2024)
-print(my_leaf.get_descriptive_name())
-my_leaf.battery.describe_battery()
-my_leaf.battery.get_range()
-my_leaf.battery.upgrade_battery()
-my_leaf.battery.get_range()
+# my_leaf = ElectricCar('nissan', 'leaf', 2024)
+# print(my_leaf.get_descriptive_name())
+# my_leaf.battery.describe_battery()
+# my_leaf.battery.get_range()
+# my_leaf.battery.upgrade_battery()
+# my_leaf.battery.get_range()
 
 # my_new_car = Car('audi', 'a4', 2024)
 # print(my_new_car.get_descriptive_name())
---
+++ Classes/my_car.py
@@ -0,0 +1,7 @@
+import cars
+
+my_new_car = cars.Car('audi', 'a4', 2024)
+print(my_new_car.get_descriptive_name())
+
+my_leaf = cars.ElectricCar('nissan', 'leaf', 2024)
+print(my_leaf.get_descriptive_name())
